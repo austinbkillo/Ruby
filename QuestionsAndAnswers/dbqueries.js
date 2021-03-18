5c689d6f48e945239887a987aaf8e126657ebc49
Austin Killough <austinbkillough@gmail.com>
2021-03-18 16:57:32 -0700

    Sets up report db function

---
+++ QuestionsAndAnswers/dbqueries.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const questionSchema = new Schema({
+  id: Number,
+  product_id: Number,
+  body: String,
+  date_written: Date,
+  asker_name: String,
+  asker_email: String,
+  helpful: Number,
+  reported: Number,
+});
+
+// const answerSchema = new Schema({
+//   id: ObjectId,
+//   question_id: ObjectId,
+//   body: String,
+//   date_written: Date,
+//   answerer_name: String,
+//   answerer_email: String,
+//   reported: Boolean,
+//   helpfulness: Number
+// })
+
+const Question = mongoose.model('Question', questionSchema);
+
+//Get all Questions
+const getAllQuestions = (productID) => {
+  Question.find({"product_id": productID})
+}
+
+//Mark Question as reported
+const markQuestionReported = (questionID) => {
+  const filter = {id: questionID};
+  const update = {reported: 1}
+  let query = Question.updateOne(filter, update).then();
+  return query;
+}
+
+module.exports = {
+  markQuestionReported: markQuestionReported
+}
--- QuestionsAndAnswers/index.js
+++ QuestionsAndAnswers/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const db = require('./dbqueries');
+mongoose.connect("/mongodb://127.0.0.1:27017/SDC", {useNewUrlParser: true})
+
+mongoose.connection
+  .on('open', (() => {console.log('connected')}))
+  .on('error', (error) => {
+    console.log(error)
+  })
+
 //Get questions
 app.get('/', (req, res) => {
   console.log('receiving')
@@ -22,6 +32,30 @@ app.post('/', (req, res) => {
   console.log('receiving')
   res.send('senderoni')
 })
+//Mark Question Reported
+app.put('/questions/report/:id', (req, res) => {
+  console.log(req.params.id)
+  const result = db.markQuestionReported(req.params.id).then((data)=>{
+    console.log(data);
+    res.send(data)});
+})
+//Mark Answer Helpful
+app.put('/', (req, res) => {
+  console.log('receiving')
+  res.send('senderoni')
+})
+//Mark Question Reported
+app.put('/questions/reported', (req, res) => {
+  console.log('receiving')
+  res.send('senderoni')
+})
+//Mark Answer Reported
+app.put('/', (req, res) => {
+  console.log('receiving')
+  res.send('senderoni')
+})
+
+
 app.listen(port, ()=>{
   console.log('listening at ' + port);
 })
